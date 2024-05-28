fb0526636469fcdc4b7a95e4dc0a7391b05d8f8d
Noah Sehman <noah.sehman@gmail.com>
2024-05-27 18:21:27 -0700

    I don't like comments. Can't let my code be readable, now can I?

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
-router.use('/api', apiRoutes); // Ensure '/api' prefix is used correctly
+router.use('/api', apiRoutes);
 
 router.use((req, res) => {
-  res.send("wrong route!"); // Catch-all for undefined routes
+  res.send("wrong route!");
 });
 
 module.exports = router;
